17b7cb77d5279d3edf7c8ad1c43fb12f597c145c
vedranpetrusic33-bit <vedranpetrusic33@gmail.com>
2026-08-25 13:59:51 +0200

    Create Product model

---
+++ src/Model/Product.php
@@ -0,0 +1,13 @@
+<?php
+
+class Product 
+{
+    public string $brand ;
+    public string $model;
+    public float $price;
+    public int $year;
+    public string $color;
+    public int $mileage;
+}
+
+?>
